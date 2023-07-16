e74d34fdf01977d2e7d665931c7e48132fea588f
Sachin M K <sachinmk27@gmail.com>
2023-07-16 22:21:27 +0530

    Add binary search variation

--- binary-search/index.js
+++ binary-search/index.js
@@ -22,6 +22,23 @@ function search(arr, target) {
   }
   return -1;
 }
+
+function searchVariation(arr, target) {
+  let left = 0;
+  let right = arr.length;
+  while (left < right) {
+    let mid = left + Math.floor((right - left) / 2);
+    console.log({ left, right, mid });
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      left = mid + 1;
+    } else if (arr[mid] > target) {
+      right = mid - 1;
+    }
+  }
+  return num[left] === target ? left : -1;
+}
 let arr = generateRandomSortedArray(1);
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log(arr);
